0373cf1bafd76ce950053ca41a450cd772f8799b
thinkie72 <tyler.hinkie@menloschool.org>
2025-02-13 20:39:47 -0800

    Work Log Entry 6

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -24,31 +24,50 @@ public class MakingChange {
             possibleCoins[i] = coins[length - i - 1];
         }
 
-//        return findWayTabulation(0, target);
+        return findWayTabulation(0, target);
 
-        memoization = new long[length][target + 1];
-        return findWayMemoization(0, target);
+//        memoization = new long[length][target + 1];
+//        return findWayMemoization(0, target);
     }
 
-    private static long findWayMemoization(int index, int remaining) {
-        if (remaining == 0) return 1;
-        if (remaining < 0 || index == memoization.length) return 0;
-
-        if (memoization[index][remaining] != 0) return memoization[index][remaining];
-
-        long include = findWayMemoization(index, remaining - possibleCoins[index]);
-        long exclude = findWayMemoization(index + 1, remaining);
-
-        memoization[index][remaining] = include + exclude;
-
-        return memoization[index][remaining];
-    }
-
-//    private static long findWayTabulation(int index, int target) {
-//        int[][] tabulation = new int[possibleCoins.length][target + 1];
+//    private static long findWayMemoization(int index, int remaining) {
+//        if (remaining == 0) return 1;
+//        if (remaining < 0 || index == memoization.length) return 0;
 //
-//        for (int i = 0; i < tabulation.length; i++) {
-//            tabulation[i][0] = 1;
-//        }
+//        if (memoization[index][remaining] != 0) return memoization[index][remaining];
+//
+//        long include = findWayMemoization(index, remaining - possibleCoins[index]);
+//        long exclude = findWayMemoization(index + 1, remaining);
+//
+//        memoization[index][remaining] = include + exclude;
+//
+//        return memoization[index][remaining];
 //    }
+
+    private static long findWayTabulation(int index, int target) {
+        long[][] tabulation = new long[possibleCoins.length][target + 1];
+
+        for (int i = 0; i < tabulation.length; i++) {
+            tabulation[i][0] = 1;
+        }
+
+        int include;
+        int exclude;
+
+        for (int i = 0; i < tabulation.length; i++) {
+            for (int j = 1; j < tabulation[0].length; j++) {
+                include = i - 1;
+                exclude = j - possibleCoins[i];
+                if (include >= 0) {
+                    tabulation[i][j] += tabulation[include][j];
+                }
+
+                if (exclude >= 0) {
+                    tabulation[i][j] += tabulation[i][exclude];
+                }
+            }
+        }
+
+        return tabulation[tabulation.length - 1][tabulation[0].length - 1];
+    }
 }
